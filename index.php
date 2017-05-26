d3e1d234ee7ba6c9648c8455f3bab9080b18dc4e
lucode <676692273@qq.com>
2017-05-26 15:51:11 +0800

    初始化完成

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucode
+ * Date: 2017/5/26
+ * Time: 15:37
+ */
+// 设置为开发者模式，默认为false 产品上线的时候使用
+define('APP_DEBUG',true);
+//导入 tp框架
+include("../ThinkPHP/ThinkPHP.php");
